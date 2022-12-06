8ecae571a53fb8387308270b716d6a446d838e4f
u66070dd <daria.dinu@student.manchester.ac.uk>
2022-12-06 20:47:28 +0000

    Connecting to firebase db

---
+++ src/components/Header.js
@@ -0,0 +1,3 @@
+export const Header = () => {
+  return <h1>Guess the sign!</h1>;
+};
---
+++ src/pages/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../utils/firebase";
+import { Header } from "../components/Header";
+import { Box, Button, Stack, TextField } from "@mui/material";
+
+const App = () => {
+  const [currentLetter, setCurrentLetter] = useState("A");
+  const [data, setData] = useState();
+  const [guess, setGuess] = useState("");
+
+  const getDataForLetterFromDB = async (letter) => {
+    const docRef = doc(db, "right-signs-alphabet", "letter" + letter);
+    const myDoc = await getDoc(docRef);
+    setData(myDoc.data());
+  };
+
+  const validateInput = () => {
+    if (guess === currentLetter) {
+      alert("Correct");
+      setCurrentLetter("B");
+    } else {
+      alert("Wrong!");
+    }
+  };
+
+  useEffect(() => {
+    getDataForLetterFromDB(currentLetter);
+  }, [currentLetter]);
+
+  return (
+    <main>
+      <div>
+        <Header />
+        <Stack>
+          <Box>
+            <TextField
+              id="outlined-basic"
+              label="Enter your guess here!"
+              variant="outlined"
+              onChange={(event) => {
+                setGuess(event.target.value);
+              }}
+            />
+            <Button variant="contained" onClick={validateInput}>
+              Verify guess
+            </Button>
+          </Box>
+
+          {data && <img style={{ width: "20%" }} src={data.url}></img>}
+        </Stack>
+      </div>
+    </main>
+  );
+};
+
+export default App;
