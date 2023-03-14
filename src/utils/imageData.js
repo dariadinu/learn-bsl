f9abb58e99c3a3fc8dc62926286303a906a39936
u66070dd <daria.dinu@student.manchester.ac.uk>
2023-03-14 14:50:28 +0000

    please

--- src/utils/imageData.js
+++ src/utils/imageData.js
@@ -1,184 +1,184 @@
 export const allImages = [
   {
     id: "black",
-    path: require("/usr/local/bin/learn-bsl/src/images/colours/black.png"),
+    path: require("../images/colours/black.png"),
     type: "colour",
   },
   {
     id: "blue",
-    path: require("/usr/local/bin/learn-bsl/src/images/colours/blue.png"),
+    path: require("../images/colours/blue.png"),
     type: "colour",
   },
   {
     id: "green",
-    path: require("/usr/local/bin/learn-bsl/src/images/colours/green.png"),
+    path: require("../images/colours/green.png"),
     type: "colour",
   },
   {
     id: "orange",
-    path: require("/usr/local/bin/learn-bsl/src/images/colours/orange.png"),
+    path: require("../images/colours/orange.png"),
     type: "colour",
   },
   {
     id: "pink",
-    path: require("/usr/local/bin/learn-bsl/src/images/colours/pink.png"),
+    path: require("../images/colours/pink.png"),
     type: "colour",
   },
   {
     id: "white",
-    path: require("/usr/local/bin/learn-bsl/src/images/colours/white.png"),
+    path: require("../images/colours/white.png"),
     type: "colour",
   },
   {
     id: "yellow",
-    path: require("/usr/local/bin/learn-bsl/src/images/colours/yellow1.png"),
+    path: require("../images/colours/yellow1.png"),
     type: "colour",
   },
   {
     id: "brother",
-    path: require("/usr/local/bin/learn-bsl/src/images/family/brother.png"),
+    path: require("../images/family/brother.png"),
     type: "family",
   },
   {
     id: "daughter",
-    path: require("/usr/local/bin/learn-bsl/src/images/family/daughter.png"),
+    path: require("../images/family/daughter.png"),
     type: "family",
   },
   {
     id: "family",
-    path: require("/usr/local/bin/learn-bsl/src/images/family/family.png"),
+    path: require("../images/family/family.png"),
     type: "family",
   },
   {
     id: "father",
-    path: require("/usr/local/bin/learn-bsl/src/images/family/father.png"),
+    path: require("../images/family/father.png"),
     type: "family",
   },
   {
     id: "grand",
-    path: require("/usr/local/bin/learn-bsl/src/images/family/grand.png"),
+    path: require("../images/family/grand.png"),
     type: "family",
   },
   {
     id: "mother",
-    path: require("/usr/local/bin/learn-bsl/src/images/family/mum.png"),
+    path: require("../images/family/mum.png"),
     type: "family",
   },
   {
     id: "sister",
-    path: require("/usr/local/bin/learn-bsl/src/images/family/sister.png"),
+    path: require("../images/family/sister.png"),
     type: "family",
   },
   {
     id: "son",
-    path: require("/usr/local/bin/learn-bsl/src/images/family/son.png"),
+    path: require("../images/family/son.png"),
     type: "family",
   },
   {
     id: "afternoon",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/afternoon.png"),
+    path: require("../images/greetings/afternoon.png"),
     type: "greetings",
   },
   {
     id: "bye",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/bye.png"),
+    path: require("../images/greetings/bye.png"),
     type: "greetings",
   },
   {
     id: "good",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/good.png"),
+    path: require("../images/greetings/good.png"),
     type: "greetings",
   },
   {
     id: "hello",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/hello.png"),
+    path: require("../images/greetings/hello.png"),
     type: "greetings",
   },
   {
     id: "howareyou",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/howareyou.png"),
+    path: require("../images/greetings/howareyou.png"),
     type: "greetings",
   },
   {
     id: "me",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/me.png"),
+    path: require("../images/greetings/me.png"),
     type: "greetings",
   },
   {
     id: "meet",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/meet.png"),
+    path: require("../images/greetings/meet.png"),
     type: "greetings",
   },
   {
     id: "name",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/name.png"),
+    path: require("../images/greetings/name.png"),
     type: "greetings",
   },
   {
     id: "nameyouwhat",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/name-you-what.png"),
+    path: require("../images/greetings/name-you-what.png"),
     type: "greetings",
   },
   {
     id: "nice",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/nice.png"),
+    path: require("../images/greetings/nice.png"),
     type: "greetings",
   },
   {
     id: "night",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/night.png"),
+    path: require("../images/greetings/night.png"),
     type: "greetings",
   },
   {
     id: "sorry",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/sorry.png"),
+    path: require("../images/greetings/sorry.png"),
     type: "greetings",
   },
   {
     id: "thanks",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/thanks.png"),
+    path: require("../images/greetings/thanks.png"),
     type: "greetings",
   },
   {
     id: "you",
-    path: require("/usr/local/bin/learn-bsl/src/images/greetings/you.png"),
+    path: require("../images/greetings/you.png"),
     type: "greetings",
   },
 ];
 export const colours = [
   {
     id: "black",
-    path: "/usr/local/bin/learn-bsl/src/images/colours/black.png",
+    path: "../images/colours/black.png",
     type: "colour",
   },
   {
     id: "blue",
-    path: "/usr/local/bin/learn-bsl/src/images/colours/blue.png",
+    path: "../images/colours/blue.png",
     type: "colour",
   },
   {
     id: "green",
-    path: "/usr/local/bin/learn-bsl/src/images/colours/green.png",
+    path: "../images/colours/green.png",
     type: "colour",
   },
   {
     id: "orange",
-    path: "/usr/local/bin/learn-bsl/src/images/colours/orange.png",
+    path: "../images/colours/orange.png",
     type: "colour",
   },
   {
     id: "pink",
-    path: "/usr/local/bin/learn-bsl/src/images/colours/pink.png",
+    path: "../images/colours/pink.png",
     type: "colour",
   },
   {
     id: "white",
-    path: "/usr/local/bin/learn-bsl/src/images/colours/white.png",
+    path: "../images/colours/white.png",
     type: "colour",
   },
   {
     id: "yellow",
-    path: "/usr/local/bin/learn-bsl/src/images/colours/yellow.png",
+    path: "../images/colours/yellow.png",
     type: "colour",
   },
 ];
@@ -186,42 +186,42 @@ export const colours = [
 export const family = [
   {
     id: "brother",
-    path: "/usr/local/bin/learn-bsl/src/images/family/brother.png",
+    path: "../images/family/brother.png",
     type: "family",
   },
   {
     id: "daughter",
-    path: "/usr/local/bin/learn-bsl/src/images/family/daughter.png",
+    path: "../images/family/daughter.png",
     type: "family",
   },
   {
     id: "family",
-    path: "/usr/local/bin/learn-bsl/src/images/family/family.png",
+    path: "../images/family/family.png",
     type: "family",
   },
   {
     id: "father",
-    path: "/usr/local/bin/learn-bsl/src/images/family/father.png",
+    path: "../images/family/father.png",
     type: "family",
   },
   {
     id: "grand",
-    path: "/usr/local/bin/learn-bsl/src/images/family/grand.png",
+    path: "../images/family/grand.png",
     type: "family",
   },
   {
     id: "mother",
-    path: "/usr/local/bin/learn-bsl/src/images/family/mom.png",
+    path: "../images/family/mom.png",
     type: "family",
   },
   {
     id: "sister",
-    path: "/usr/local/bin/learn-bsl/src/images/family/sister.png",
+    path: "../images/family/sister.png",
     type: "family",
   },
   {
     id: "son",
-    path: "/usr/local/bin/learn-bsl/src/images/family/son.png",
+    path: "../images/family/son.png",
     type: "family",
   },
 ];
@@ -229,72 +229,72 @@ export const family = [
 export const greetings = [
   {
     id: "afternoon",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/afternoon.png",
+    path: "../images/greetings/afternoon.png",
     type: "greetings",
   },
   {
     id: "bye",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/bye.png",
+    path: "../images/greetings/bye.png",
     type: "greetings",
   },
   {
     id: "good",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/good.png",
+    path: "../images/greetings/good.png",
     type: "greetings",
   },
   {
     id: "hello",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/hello.png",
+    path: "../images/greetings/hello.png",
     type: "greetings",
   },
   {
     id: "howareyou",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/howareyou.png",
+    path: "../images/greetings/howareyou.png",
     type: "greetings",
   },
   {
     id: "me",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/me.png",
+    path: "../images/greetings/me.png",
     type: "greetings",
   },
   {
     id: "meet",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/meet.png",
+    path: "../images/greetings/meet.png",
     type: "greetings",
   },
   {
     id: "name",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/name.png",
+    path: "../images/greetings/name.png",
     type: "greetings",
   },
   {
     id: "nameyouwhat",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/nameyouwhat.png",
+    path: "../images/greetings/nameyouwhat.png",
     type: "greetings",
   },
   {
     id: "nice",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/nice.png",
+    path: "../images/greetings/nice.png",
     type: "greetings",
   },
   {
     id: "night",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/night.png",
+    path: "../images/greetings/night.png",
     type: "greetings",
   },
   {
     id: "sorry",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/sorry.png",
+    path: "../images/greetings/sorry.png",
     type: "greetings",
   },
   {
     id: "thanks",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/thanks.png",
+    path: "../images/greetings/thanks.png",
     type: "greetings",
   },
   {
     id: "you",
-    path: "/usr/local/bin/learn-bsl/src/images/greetings/you.png",
+    path: "../images/greetings/you.png",
     type: "greetings",
   },
 ];
@@ -323,48 +323,48 @@ export const familyLessonSteps = [
     label: "Family",
     description:
       "Fingerspelled 'F' makes small clockwise circles in front of body.",
-    imagepath: require("/usr/local/bin/learn-bsl/src/images/family/family.png"),
+    imagepath: require("../images/family/family.png"),
   },
   {
     label: "Mother",
     description: ` Extended index, middle and ring fingers of primary hand tap extended index and middle 
         fingers of secondary hand twice = Fingerspelled 'M' taps palm twice.`,
-    imagepath: require("/usr/local/bin/learn-bsl/src/images/family/mum.png"),
+    imagepath: require("../images/family/mum.png"),
   },
   {
     label: "Father",
     description:
       " Extended index and middle fingers of primary hand tap extended index and middle fingers of secondary hand twice = Fingerspelled 'F' twice",
-    imagepath: require("/usr/local/bin/learn-bsl/src/images/family/father.png"),
+    imagepath: require("../images/family/father.png"),
   },
   {
     label: "Brother",
     description:
       "Both closed hands brush up and down past each other (touching each other) in front of body.",
-    imagepath: require("/usr/local/bin/learn-bsl/src/images/family/brother.png"),
+    imagepath: require("../images/family/brother.png"),
   },
   {
     label: "Sister",
     description: "Hooked index finger of closed hand taps nose twice.",
-    imagepath: require("/usr/local/bin/learn-bsl/src/images/family/sister.png"),
+    imagepath: require("../images/family/sister.png"),
   },
 
   {
     label: "Son",
     description:
       " Extended index finger of primary hand brushes along chin (from primary side to secondary side).",
-    imagepath: require("/usr/local/bin/learn-bsl/src/images/family/son.png"),
+    imagepath: require("../images/family/son.png"),
   },
   {
     label: "Daughter",
     description: "Fingerspelled 'D' twice.",
-    imagepath: require("/usr/local/bin/learn-bsl/src/images/family/daughter.png"),
+    imagepath: require("../images/family/daughter.png"),
   },
   {
     label: "Grand",
     description:
       "To form compound words such as 'Grandfather', 'Grandmother', 'Granddaughter' or 'Grandson', firstly we sign the letter 'G' and then proceed to sign the respective word after 'Grand'.",
-    imagepath: require("/usr/local/bin/learn-bsl/src/images/family/grand.png"),
+    imagepath: require("../images/family/grand.png"),
   },
 ];
 
@@ -377,46 +377,46 @@ export const familyLessonQuizQuestions = {
     {
       question: "What is the sign for 'Mother'?",
       choicesImg: [
-        require("/usr/local/bin/learn-bsl/src/images/family/daughter.png"),
+        require("../images/family/daughter.png"),
 
-        require("/usr/local/bin/learn-bsl/src/images/family/family.png"),
+        require("../images/family/family.png"),
 
-        require("/usr/local/bin/learn-bsl/src/images/family/son.png"),
+        require("../images/family/son.png"),
 
-        require("/usr/local/bin/learn-bsl/src/images/family/mum.png"),
+        require("../images/family/mum.png"),
       ],
       type: "MCQs",
-      correctAnswer: require("/usr/local/bin/learn-bsl/src/images/family/mum.png"),
-      // imagePath: require("/usr/local/bin/learn-bsl/src/images/family/daughter.png"),
+      correctAnswer: require("../images/family/mum.png"),
+      // imagePath: require("../images/family/daughter.png"),
     },
     {
       question: "What is this image sign for?",
       choices: ["father", "mother", "daughter", "son"],
       type: "MCQs",
       correctAnswer: "daughter",
-      imagePath: require("/usr/local/bin/learn-bsl/src/images/family/daughter.png"),
+      imagePath: require("../images/family/daughter.png"),
     },
     {
       question: "What is this image sign for?",
       choices: ["family", "grand", "sister", "none of the above"],
       type: "MCQs",
       correctAnswer: "sister",
-      imagePath: require("/usr/local/bin/learn-bsl/src/images/family/sister.png"),
+      imagePath: require("../images/family/sister.png"),
     },
     {
       question: "What is the sign for 'Brother'?",
       choicesImg: [
-        require("/usr/local/bin/learn-bsl/src/images/family/brother.png"),
+        require("../images/family/brother.png"),
 
-        require("/usr/local/bin/learn-bsl/src/images/family/mum.png"),
+        require("../images/family/mum.png"),
 
-        require("/usr/local/bin/learn-bsl/src/images/family/son.png"),
+        require("../images/family/son.png"),
 
-        require("/usr/local/bin/learn-bsl/src/images/family/family.png"),
+        require("../images/family/family.png"),
       ],
       type: "MCQs",
-      correctAnswer: require("/usr/local/bin/learn-bsl/src/images/family/brother.png"),
-      // imagePath: require("/usr/local/bin/learn-bsl/src/images/family/daughter.png"),
+      correctAnswer: require("../images/family/brother.png"),
+      // imagePath: require("../images/family/daughter.png"),
     },
   ],
 };
